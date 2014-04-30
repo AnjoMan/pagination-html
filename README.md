d7b5a4d00ed2cc82be070a8cac69ae16e7db077e
anton <antonlodder@gmail.com>
2014-04-30 10:22:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+**Paginated Web App**
+
+This repository contains a working web app with left-right pagination. It is designed using CSS3 and JQuery and supports keyboard interaction for left and right scrolling from page to page. The working example is of an expense tracking app for mobile, which makes it easy to scroll through a simple form and input information.
---
+++ js/navigate.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+	$(document.documentElement).keyup(function (event) { // handle cursor keys
+	  if (event.keyCode == 37) { moveBackward() }// go left
+	  else if (event.keyCode == 39) {
+	    moveForward()}// go right 
+	});
+
+	infoDivs = $('div.info')//this is used in moveForward, moveBackward	
+
+
+	// initialize to first div.info
+	$(infoDivs).hide().css('left',$('.nav-els').width())
+	$(infoDivs[0]).show().addClass('active').animate({left:0},200)
+
+
+
+	// add click handlers to forward/backward buttons
+	$('div#forward').click( moveForward)
+	$('div#back').click( moveBackward)
+
+
+
+
+
+
+
+	function moveForward(){//move forwards in the list of .info divs
+		var activeIndex = getActive(infoDivs)
+
+		if (activeIndex+1 < infoDivs.length){
+			moveAnimate(infoDivs[activeIndex], infoDivs[activeIndex+1], -$('.nav-els').width())
+		}
+		return true;
+
+	}
+
+	function moveBackward(){//move backwards in the list of .info divs
+		var activeIndex = getActive(infoDivs)
+		if (activeIndex-1 >= 0){
+			moveAnimate(infoDivs[activeIndex], infoDivs[activeIndex-1], $('.nav-els').width())
+		}
+		return true
+	}
+
+	function moveAnimate(oldEl, newEl, displace){
+		// move old element out of the way and hide
+		$(oldEl).animate({left: displace},200,function(){
+			$(this).hide().removeClass('active')})
+		// move new element into the center
+		$(newEl)
+			.show().addClass('active')
+			.animate({left: 0},200)
+
+	}
+	function getActive(els){ // find the index of the active div
+		for(i=0; i< els.length; i++){
+			if( $(els[i]).hasClass('active')){ return i }
+		}
+	};
+});
